96d99045b25bbfa9a6a88ca3e6623cbe7fc0c39d
23conniej <56559999+23conniej@users.noreply.github.com>
2019-10-21 11:23:47 -0700

    Update Fence.java

--- Fence.java
+++ Fence.java
@@ -1,4 +1,31 @@
+/**
+ * @author Connie Jiang
+ */
 
-public class Fence {
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
 
+
+public class Fence extends GamePiece{
+	private static BufferedImage sprite;
+	
+	public Fence(int x, int y) {
+		super(x, y);
+	}
+	
+	public void drawFence(int xcoord, int ycoord, Graphics g) {
+		try {
+	    	sprite = ImageIO.read(new File("src/fence.png"));
+	    }
+	    catch (IOException e) {
+	    	System.out.println("Exception caught");
+	    }
+		g.drawImage(sprite, xcoord, ycoord, 50, 50, null);
+	}
 }
