4d55be537a515e1ba42971d90984d1c1de940416
Sergey Churyukin <sergey@churyukin.com>
2019-10-21 11:10:48 -0700

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,4 +1,14 @@
 
 public class Grid {
-
+  GamePiece[][] field = new GamePiece[12][12];
+  
+  for (int i = 0; i < 12; i++) {
+    for (int j = 0; i < 12; i++) {
+      if (i == 0 || i == 11) {
+        field[i][j] = You();
+      }
+    }
+  }
+    
+  
 }
