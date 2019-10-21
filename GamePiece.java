fd8502d04ee902a7a678376ad1673547fe021a9b
23conniej <56559999+23conniej@users.noreply.github.com>
2019-10-21 11:26:05 -0700

    Update GamePiece.java

--- GamePiece.java
+++ GamePiece.java
@@ -4,5 +4,36 @@
  *
  */
 public class GamePiece {
-
+	private int x;
+	private int y;
+	
+	public GamePiece() {
+		
+	}
+	
+	public GamePiece(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int coord) {
+		x = coord;
+	}
+	
+	public void setY(int coord) {
+		y = coord;
+	}
+	
+	public void move(int X, int Y) {
+		x = X;
+		y = Y;
+	}
 }
