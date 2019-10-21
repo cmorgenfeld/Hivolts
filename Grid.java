aa0ddb694d7f86840eb8d3ea8d059f8fdcbd651e
Sergey Churyukin <sergey@churyukin.com>
2019-10-21 11:20:33 -0700

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -5,13 +5,13 @@ public class Grid {
   for (int i = 0; i < 12; i++) {
     for (int j = 0; j < 12; j++) {
       if (i == 0 || i == 11) {
-        field[i][j] = Fence(i, j);
+        field[i][j] = new Fence(i, j);
       }
       if (j == 0 || j == 11) {
-        field[i][j] = Fence(i, j);
+        field[i][j] = new Fence(i, j);
       }
       else if (Math.random() >= 0.2 && fenceCount < 20) {
-        field[i][j] = Fence(i, j);
+        field[i][j] = new Fence(i, j);
         fenceCount++;
       }
     }
