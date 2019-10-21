b691a044bf69e74d0a7aa12cb6905854e4eea1e5
23conniej <56559999+23conniej@users.noreply.github.com>
2019-10-21 10:28:27 -0700

    Add files via upload

--- MainDraw.java
+++ MainDraw.java
@@ -1,19 +1,29 @@
-package game;
-
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JComponent;
+import javax.swing.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
 
 public class MainDraw extends JComponent {
 
     public int x = 50;
     public int y = 50;
+    public static BufferedImage image;
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawRect(x, y, 50, 50);
-        g.fillRect(x, y, 50, 50);
-        g.setColor(Color.BLACK);
+        try {
+        	image = ImageIO.read(new File("src/fence.png"));
+        }
+        catch (IOException e) {
+        	System.out.println("Exception caught");
+        }
+        g.drawImage(image, 0, 0, 30, 30, Color.WHITE, null);
+        //g.fillRect(x, y, 50, 50);
+        //g.setColor(Color.BLACK);
     }
 
     public void moveRight() {
