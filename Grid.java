5f7fafc6d7983af98399539950d162cd6ce25353
Sergey Churyukin <sergey@churyukin.com>
2019-10-21 11:15:58 -0700

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,11 +1,18 @@
 
 public class Grid {
   GamePiece[][] field = new GamePiece[12][12];
-  
+  int fenceCount = 0;
   for (int i = 0; i < 12; i++) {
-    for (int j = 0; i < 12; i++) {
+    for (int j = 0; j < 12; j++) {
       if (i == 0 || i == 11) {
-        field[i][j] = You();
+        field[i][j] = Fence(i, j);
+      }
+      if (j == 0 || j == 11) {
+        field[i][j] = Fence(i, j);
+      }
+      else if (Math.random() >= 0.2 && fenceCount < 20) {
+        field[i][j] = Fence(i, j);
+        fenceCount++;
       }
     }
   }
