86fce900e64af3330854da25c475aaaf118b56b8
23conniej <56559999+23conniej@users.noreply.github.com>
2019-10-23 10:16:47 -0700

    Update You.java

--- You.java
+++ You.java
@@ -1,4 +1,37 @@
+/**
+ 
+ * @author Connie Jiang
+ */
 
-public class You {
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
 
+public class You extends GamePiece{
+	private static BufferedImage sprite;
+	
+	public You(int x, int y) {
+		super(x, y);
+	}
+	
+	public void drawYou(int xcoord, int ycoord, Graphics g) {
+		try {
+	    	sprite = ImageIO.read(new File("src/You.png"));
+	    }
+	    catch (IOException e) {
+	    	System.out.println("Exception caught");
+	    }
+		g.drawImage(sprite, xcoord, ycoord, 50, 50, null);
+	}
+	
+	/*
+	 * 
+	public boolean move(int x, int y, boolean jump) {
+	}
+	*/
 }
