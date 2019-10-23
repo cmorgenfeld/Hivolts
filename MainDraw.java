ba70ede6d14c8a1aac652a8e971e5cc960f3235f
cmorgenfeld <cmorgenfeld@gmail.com>
2019-10-23 10:50:39 -0700

    Update MainDraw.java

--- MainDraw.java
+++ MainDraw.java
@@ -14,40 +14,33 @@ public class MainDraw extends JComponent {
 
     public int x = 50;
     public int y = 50;
-    private static BufferedImage image;
+    private static BufferedImage fence;
+    private static BufferedImage mho;
+    
+    final private Color background =  new Color(238, 238, 238, 255);
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         try {
-        	image = ImageIO.read(new File("src/game/fence.png"));
+        	fence = ImageIO.read(new File("src/game/fence.png"));
+        	mho = ImageIO.read(new File("src/game/Mho.png"));
         } catch (IOException e) {
         	System.out.println("Exception caught");
         }
         for(int i=0; i<600; i+= 50) {
-        	g.drawImage(image, i, 0, 50, 50, Color.WHITE, null);
-        	g.drawImage(image, i, 550, 50, 50, Color.WHITE, null);
-        	g.drawImage(image, 0, i, 50, 50, Color.WHITE, null);
-        	g.drawImage(image, 550, i, 50, 50, Color.WHITE, null);
+        	g.drawImage(fence, i, 0, 50, 50, background, null);
+        	g.drawImage(fence, i, 550, 50, 50, background, null);
+        	g.drawImage(fence, 0, i, 50, 50, background, null);
+        	g.drawImage(fence, 550, i, 50, 50, background, null);
         }
+        g.drawImage(mho, 100, 100, 50, 50, background, null);
+        drawGrid(g);
     }
 
-    public void moveRight() {
-        x = x + 5;
-        repaint();
-    }
-
-    public void moveLeft() {
-        x = x - 5;
-        repaint();
-    }
-
-    public void moveDown() {
-        y = y + 5;
-        repaint();
-    }
-
-    public void moveUp() {
-        y = y - 5;
-        repaint();
+    public void drawGrid (Graphics g) {
+    	for(int i=50; i<600; i+= 50) {
+    		g.drawLine(i, 0, i, 600);
+    		g.drawLine(0, i, 600, i);
+    	}
     }
 }
