36aa8d0228ee75092dbefef0a16f0adf27688595
23conniej <56559999+23conniej@users.noreply.github.com>
2019-10-23 10:44:15 -0700

    Update Mho.java

--- Mho.java
+++ Mho.java
@@ -1,4 +1,45 @@
+/**
+ * 
+ * @author Connie Jiang
+ *
+ */
 
-public class Mho {
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JComponent;
 
+import javax.swing.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+
+public class Mho extends GamePiece{
+	private static BufferedImage sprite;
+	
+	public Mho(int x, int y) {
+		super(x, y);
+	}
+	
+	public void drawYou(int xcoord, int ycoord, Graphics g) {
+		try {
+	    	sprite = ImageIO.read(new File("src/Mho.png"));
+	    }
+	    catch (IOException e) {
+	    	System.out.println("Exception caught");
+	    }
+		g.drawImage(sprite, xcoord, ycoord, 50, 50, null);
+	}
+	
+	public void move(int x, int y) {
+		super.setX(x);
+		super.setY(y);
+		isDead();
+	}
+	
+	public void isDead() {
+		if(Grid.field[this.getX()][this.getY()] instanceof Fence) {
+			setDead(true);
+		}
+	}
 }
