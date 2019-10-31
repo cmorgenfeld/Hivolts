249c6a725ef8b4db9050894e0b464b495771973f
cmorgenfeld <cmorgenfeld@gmail.com>
2019-10-31 11:29:15 -0700

    Update Mho.java

--- Mho.java
+++ Mho.java
@@ -2,81 +2,52 @@ package game;
 
 import java.awt.Color;
 import java.awt.Graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+
 import javax.swing.JComponent;
 
-import javax.swing.*;
-import java.io.*;
-import java.awt.*;
-import java.awt.image.*;
 import javax.imageio.*;
 
-public class Mho extends GamePiece{
-	private static BufferedImage sprite;
-	private int number;
-	
-	public Mho(int x, int y, int number) {
-		super(x, y);
-		this.number = number;
-	}
-	
-	public static void drawMho(int xcoord, int ycoord, Graphics g) {
-		try {
-	    	sprite = ImageIO.read(new File("src/game/Mho.png"));
-	    }
-	    catch (IOException e) {
-	    	System.out.println("Exception caught");
-	    }
-		for(int i=0; i<11; i++) {
-			for(int j=0; j<11; j++) {
+public class MainDraw extends JComponent {
+
+    public int x = 50;
+    public int y = 50;
+    public You you;
+    private Mho thisMho;
+    
+    final private Color background =  new Color(238, 238, 238, 255);
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        
+        int[] mhoList = new int[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+		for(int i=0; i<12; i++) {
+			for(int j=0; j<12; j++) {
 				if(Grid.field[i][j] instanceof Mho) {
-					Grid.field[i][j].move(i, j);
-				}
-			}
-		}
-		g.drawImage(sprite, xcoord, ycoord, 50, 50, null);
-		
-	}
-	
-	public void move(int x, int y) {
-		System.out.println("move");
-		int[] youXY = wheresYou();
-		int diffX = Math.abs(x - youXY[0]);
-		int diffY = Math.abs(y - youXY[1]);
-		if(diffX > diffY) {
-			if(youXY[0] > x) {
-				super.setX(x + 1);
-			} else {
-				super.setX(x - 1);
-			}
-		} else if (diffY > diffX) {
-			if(youXY[1] > y) {
-				super.setY(y + 1);
-			} else {
-				super.setY(y - 1);
-			}
-		} else {
-			//Diagonal
-		}
-		isDead();
-	}
-	
-	public int[] wheresYou() {
-		int[] retval = new int[2];
-		for(int i=0; i<11; i++) {
-			for(int j=0; j<11; j++) {
-				if(Grid.field[i][j] instanceof You) {
-					retval[0] = i;
-					retval[1] =j;
-					break;
-				}
-			}
-		}
-		return retval;
-	}
-	
-	public void isDead() {
-		if(Grid.field[this.getX()][this.getY()] instanceof Fence) {
-			setDead(true);
-		}
-	}
+					thisMho = (Mho) Grid.field[i][j];
+					if (thisMho.getDead()) {
+						Grid.field[i][j] = null;
+					} else if(mhoList[thisMho.getNumber()] == 1) {
+	        			thisMho.move(i, j);
+		        		mhoList[thisMho.getNumber()] = 0;
+		        		Mho.drawMho(i*50, j*50, g);
+	        		}
+	        	} else if(Grid.field[i][j] instanceof Fence) {
+	        		Fence.drawFence(i*50, j*50, g);
+	        	}
+			} 	
+        }
+        you.drawYou(g);
+        drawGrid(g);
+    }
+
+    public void drawGrid (Graphics g) {
+    	for(int i=50; i<600; i+= 50) {
+    		g.drawLine(i, 0, i, 600);
+    		g.drawLine(0, i, 600, i);
+    	}
+    }
 }
