d1209162e0cf891efe7c4c609c0d4d978c1cb316
23conniej <56559999+23conniej@users.noreply.github.com>
2019-11-03 13:00:08 -0800

    Update Fence.java

--- Fence.java
+++ Fence.java
@@ -1,5 +1,3 @@
-package game;
-
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JComponent;
@@ -9,17 +7,33 @@ import java.awt.*;
 import java.awt.image.*;
 import javax.imageio.*;
 
-
+/**
+ * this Fence class is a GamePiece that can draw itself (a png image of an electric fence)
+ * @author Connie Jiang
+ * 			documentation by Jeffrey
+ *
+ */
 public class Fence extends GamePiece{
 	private static BufferedImage sprite;
 	
+	/**
+	 * constructor for Fence
+	 * @param x initial x coordinate
+	 * @param y initial y coordinate
+	 */
 	public Fence(int x, int y) {
 		super(x, y);
 	}
 	
+	/**
+	 * draws a fence
+	 * @param xcoord x coordinate of the image
+	 * @param ycoord y coordinate of the image
+	 * @param g the Graphics variable
+	 */
 	public static void drawFence(int xcoord, int ycoord, Graphics g) {
 		try {
-	    	sprite = ImageIO.read(new File("src/game/fence.png"));
+	    	sprite = ImageIO.read(new File("src/fence.png"));
 	    }
 	    catch (IOException e) {
 	    	System.out.println("Exception caught");
@@ -27,4 +41,3 @@ public class Fence extends GamePiece{
 		g.drawImage(sprite, xcoord, ycoord, 50, 50, null);
 	}
 }
-
