561c8be8f7e53a3a4db54b83707dd046c1eeeb62
cmorgenfeld <cmorgenfeld@gmail.com>
2019-11-04 10:57:54 -0800

    Update MainDraw.java

--- MainDraw.java
+++ MainDraw.java
@@ -26,24 +26,29 @@ public class MainDraw extends JComponent {
      */
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for(int i=0; i<600; i+= 50) {
-        	Fence.drawFence(i, 0, g);
-        	Fence.drawFence(i,  550,  g);
-        	Fence.drawFence(0, i, g);
-        	Fence.drawFence(550, i, g);
-        }
-
+        
+        int[] mhoList = new int[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 		for(int i=0; i<12; i++) {
-			for(int k=0; k<12; k++) {
-				if(Grid.field[i][k] instanceof Mho) {
-	        		Mho.drawMho(i*50, k*50, g);
-	        	} else if(Grid.field[i][k] instanceof Fence) {
-	        		Fence.drawFence(i*50, k*50, g);
+			for(int j=0; j<12; j++) {
+				if(Grid.field[i][j] instanceof Mho) {
+					thisMho = (Mho) Grid.field[i][j];
+					if (thisMho.getDead()) {
+						Grid.field[i][j] = null;
+					} else if(mhoList[thisMho.getNumber()] == 1) {
+	        			thisMho.move(i, j);
+		        		mhoList[thisMho.getNumber()] = 0;
+		        		Mho.drawMho(i*50, j*50, g);
+	        		}
+	        	} else if(Grid.field[i][j] instanceof Fence) {
+	        		Fence.drawFence(i*50, j*50, g);
 	        	}
 			} 	
-        }
-        you.drawYou(g);
+        }  
         drawGrid(g);
+        you.drawYou(g);
+        if(you.getDead()) {
+        	You.deadScreen(g);
+        }
     }
 
     public void drawGrid (Graphics g) {
